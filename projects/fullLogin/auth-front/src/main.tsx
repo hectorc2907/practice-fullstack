e99480f4032a6fe96f3dec1b5a258546db8c978d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 21:58:49 -0300

    generamos 3 componentes el cual son el login, registro y el dashboard al logear

--- projects/fullLogin/auth-front/src/main.tsx
+++ projects/fullLogin/auth-front/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./routes/Login.tsx";
+import { SignUp } from "./routes/SignUp.tsx";
+import { Dashboard } from "./routes/Dashboard.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
