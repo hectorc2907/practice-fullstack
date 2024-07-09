4a6add468318d18b1f777f0197fe4192a2565425
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-09 11:37:15 -0300

    terminamos de configurar el signup desde el backend para las validaciones y guardar usuario

--- projects/fullLogin/auth-back/routes/signup.js
+++ projects/fullLogin/auth-back/routes/signup.js
@@ -1,29 +1,43 @@
-const router = require("express").Router();
-const { jsonResponse } = require("../lib/jsonResponse");
+const express = require("express");
 const User = require("../schema/user");
+const { jsonResponse } = require("../lib/jsonResponse");
+const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { username, name, password } = req.body;
+router.post("/", async function (req, res) {
+  const { username, password, name } = req.body;
 
-  if (!username || !name || !password) {
-    return res.status(400).json(
-      jsonResponse(400, {
+  if (!username || !password || !name) {
+    return res.status(409).json(
+      jsonResponse(409, {
         error: "Fields are required",
       })
     );
   }
-
-  const user = new User({
-    username,
-    name,
-    password,
-  });
-  await user.save();
-  res
-    .status(200)
-    .json(jsonResponse(200, { message: "User created successfully" }));
-
-  res.send("signup");
+  try {
+    const user = new User();
+    const exists = await user.usernameExists(username);
+    if (exists) {
+      return res.status(409).json(
+        jsonResponse(409, {
+          error: "username already exists",
+        })
+      );
+    } else {
+      const user = new User({ username, password, name });
+      await user.save();
+      res.json(
+        jsonResponse(200, {
+          message: "User created successfully",
+        })
+      );
+    }
+  } catch (error) {
+    return res.status(500).json(
+      jsonResponse(500, {
+        error: "Error creating user",
+      })
+    );
+  }
 });
 
 module.exports = router;
