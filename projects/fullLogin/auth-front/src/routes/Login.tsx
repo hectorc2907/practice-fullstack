1bcdf06e8249323e4cd4fc3cca34b9bb03a367fd
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 22:39:49 -0300

    se realiza las configuraciones del login y signup

--- projects/fullLogin/auth-front/src/routes/Login.tsx
+++ projects/fullLogin/auth-front/src/routes/Login.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import { DefaultLayout } from "../layout/DefaultLayout";
+
 export function Login() {
-  return <h1>Login</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <DefaultLayout>
+      <form className="form">
+        <h1>Login</h1>
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button>Login</button>
+      </form>
+    </DefaultLayout>
+  );
 }
--- projects/fullLogin/auth-front/src/routes/SignUp.tsx
+++ projects/fullLogin/auth-front/src/routes/SignUp.tsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+import { DefaultLayout } from "../layout/DefaultLayout";
+
 export function SignUp() {
-  return <h1>SignUp</h1>;
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <DefaultLayout>
+      <form className="form">
+        <h1>SignUp</h1>
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button>Create User</button>
+      </form>
+    </DefaultLayout>
+  );
 }
