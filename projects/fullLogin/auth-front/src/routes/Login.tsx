bde6f1c8ea005e57556b47e2f9b1f322db224ad6
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-09 01:02:39 -0300

    se configura el Login sin hacer uso de los token

--- projects/fullLogin/auth-front/src/routes/Login.tsx
+++ projects/fullLogin/auth-front/src/routes/Login.tsx
@@ -2,31 +2,74 @@ import { useState } from "react";
 import { DefaultLayout } from "../layout/DefaultLayout";
 import { useAuth } from "../auth/AuthProvider";
 import { Navigate } from "react-router-dom";
+import { AuthResponse, AuthResponseError } from "../types/types";
+
+const backend = import.meta.env.VITE_API_URL;
 
 export function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorResponse, setErrorResponse] = useState("");
   const auth = useAuth();
+
+  function handleChange(e: React.ChangeEvent) {
+    const { name, value } = e.target as HTMLInputElement;
+    if (name === "username") {
+      setUsername(value);
+    }
+    if (name === "password") {
+      setPassword(value);
+    }
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${backend}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        const json = (await response.json()) as AuthResponse;
+        console.log(json);
+
+        // if (json.body.accessToken && json.body.refreshToken) {
+        //   auth.saveUser(json);
+        // }
+      } else {
+        const json = (await response.json()) as AuthResponseError;
+        setErrorResponse(json.body.error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   if (auth.isAuthenticated) {
     return <Navigate to="/dashboard" />;
   }
   return (
     <DefaultLayout>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <h1>Login</h1>
-        <label htmlFor="">Username</label>
+        {!!errorResponse && <div className="errorMessage">{errorResponse}</div>}
+        <label>Username</label>
         <input
+          name="username"
           type="text"
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={handleChange}
         />
 
-        <label htmlFor="">Password</label>
+        <label>Password</label>
         <input
+          name="password"
           type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handleChange}
         />
 
         <button>Login</button>
