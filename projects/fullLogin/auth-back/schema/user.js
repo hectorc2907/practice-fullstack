8e9bb344f16f1749d6202280f1e97da8b022f65a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-09 10:57:03 -0300

    generamos la encriptacion de la contraseña

--- projects/fullLogin/auth-back/schema/user.js
+++ projects/fullLogin/auth-back/schema/user.js
@@ -1,8 +1,25 @@
 const { Mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Mongoose.Schema({
   id: { type: Object },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+});
+
+UserSchema.pre("save", function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const document = this;
+    bcrypt.hash(document.password, 10, (error, hash) => {
+      if (error) {
+        next(error);
+      } else {
+        document.password = hash;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 });
