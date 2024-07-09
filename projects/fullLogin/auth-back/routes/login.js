0338ca9338e6bb12f1c9e5d06f8ee7cfb7603082
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-09 10:14:12 -0300

    se realiza al login desde el backend

--- projects/fullLogin/auth-back/routes/login.js
+++ projects/fullLogin/auth-back/routes/login.js
@@ -1,7 +1,25 @@
+const { jsonResponse } = require("../lib/jsonResponse");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("login");
+router.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json(
+      jsonResponse(400, {
+        error: "Fields are required",
+      })
+    );
+  }
+
+  const accessToken = "access_token";
+  const resfreshToken = "refresh_token";
+  const user = {
+    id: "1",
+    name: "John Doe",
+    username: "XXXXXXX",
+  };
+  res.status(200).json(jsonResponse(200, { user, accessToken, resfreshToken }));
 });
 
 module.exports = router;
