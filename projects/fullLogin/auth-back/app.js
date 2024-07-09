9bec06bdf45e381685f94873cd816abc793ce38a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 23:16:33 -0300

    configuramos las rutas a utilizar

--- projects/fullLogin/auth-back/app.js
+++ projects/fullLogin/auth-back/app.js
@@ -6,6 +6,16 @@ require("dotenv").config();
 
 const port = process.env.PORT || 3100;
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/signup", require("./routes/signup"));
+app.use("/api/login", require("./routes/login"));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/refresh-token", require("./routes/refreshToken"));
+app.use("/api/logout", require("./routes/logout"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
