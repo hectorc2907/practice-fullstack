08a7a761cb1eaba031f62ca20a22599b66ddfd58
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 23:43:37 -0300

    configuramos el creador de usuario

--- projects/fullLogin/auth-back/routes/signup.js
+++ projects/fullLogin/auth-back/routes/signup.js
@@ -1,6 +1,22 @@
+const { jsonResponse } = require("../lib/jsonResponse");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  const { username, name, password } = req.body;
+
+  if (!!username || !!name || !!password) {
+    return res.status(400).json(
+      jsonResponse(400, {
+        error: "Fields are required",
+      })
+    );
+  }
+
+  res
+    .status(200)
+    .json(jsonResponse(200, { message: "User created successfully" }));
+
   res.send("signup");
 });
 
