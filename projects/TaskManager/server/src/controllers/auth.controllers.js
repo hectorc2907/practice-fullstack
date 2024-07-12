eeda6f2c6953007156bc0f9e93f0bf1affba6743
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 02:03:54 -0300

    configuramos el registro con la implementacion del token

--- projects/TaskManager/server/src/controllers/auth.controllers.js
+++ projects/TaskManager/server/src/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { email, username, password } = req.body;
@@ -8,14 +9,26 @@ export const register = async (req, res) => {
     const passwordHash = await bcrypt.hash(password, 10);
 
     const newUser = new User({ username, email, password: passwordHash });
+
     const userSaved = await newUser.save();
-    res.json({
-      id: userSaved._id,
-      username: userSaved.username,
-      email: userSaved.email,
-      createAt: userSaved.createdAt,
-      updateAt: userSaved.updatedAt,
-    });
+
+    jwt.sign(
+      { id: userSaved._id },
+      "secret123",
+      { expiresIn: "1d" },
+      (error, token) => {
+        if (error) console.error(error);
+        res.json({ token });
+      }
+    );
+
+    // res.json({
+    //   id: userSaved._id,
+    //   username: userSaved.username,
+    //   email: userSaved.email,
+    //   createAt: userSaved.createdAt,
+    //   updateAt: userSaved.updatedAt,
+    // });
   } catch (error) {
     console.error(error);
   }
