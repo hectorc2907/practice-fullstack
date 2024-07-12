3ddfe22f096d4fb40f690c03fcdf6205b6c8ffdc
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 12:47:57 -0300

    configuramos un registro de usuario basico este solo devuelve los valores ingresados

--- projects/TaskManager/client/src/pages/RegisterPage.jsx
+++ projects/TaskManager/client/src/pages/RegisterPage.jsx
@@ -1,5 +1,36 @@
+import { useForm } from "react-hook-form";
+
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const { register, handleSubmit } = useForm();
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form
+        onSubmit={handleSubmit((values) => {
+          console.log(values);
+        })}
+      >
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Username"
+        />
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Password"
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
