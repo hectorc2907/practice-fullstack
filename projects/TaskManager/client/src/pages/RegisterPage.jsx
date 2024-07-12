6be5306408c0cc0aa07f989ab8ce077f38157012
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 13:20:09 -0300

    comenzamos con la configuracion del AuthContext, envolvemos toda la App.jsx en el AuthProvider y configuramos el registro de usuario en el auth devolviendo solo la funcion signup

--- projects/TaskManager/client/src/pages/RegisterPage.jsx
+++ projects/TaskManager/client/src/pages/RegisterPage.jsx
@@ -1,12 +1,14 @@
 import { useForm } from "react-hook-form";
-import { registerRequest } from "../api/auth";
+import { useAuth } from "../context/AuthContext";
 
 function RegisterPage() {
   const { register, handleSubmit } = useForm();
+  const { signup, user } = useAuth();
+
+  console.log(user);
 
   const onSubmit = handleSubmit(async (values) => {
-    const res = await registerRequest(values);
-    console.log(res);
+    signup(values);
   });
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md">
