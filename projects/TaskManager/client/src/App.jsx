a2b9e14a111269b54ddd8d351f9f3c0f127ecd3b
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 14:57:31 -0300

    configuramos las routes y creamos el page de profile

--- projects/TaskManager/client/src/App.jsx
+++ projects/TaskManager/client/src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
 import { AuthProvider } from "./context/AuthContext";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TasksPage from "./pages/TasksPage";
+import TaskFormPage from "./pages/TaskFormPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home Page</h1>}></Route>
+          <Route path="/" element={<HomePage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
-          <Route path="/tasks" element={<h1>Tasks Page</h1>}></Route>
-          <Route path="/add-task" element={<h1>New Task</h1>}></Route>
-          <Route path="/tasks/:id" element={<h1>Update Task</h1>}></Route>
-          <Route path="/profile" element={<h1>Profile</h1>}></Route>
+          <Route path="/tasks" element={<TasksPage />}></Route>
+          <Route path="/add-task" element={<TaskFormPage />}></Route>
+          <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
