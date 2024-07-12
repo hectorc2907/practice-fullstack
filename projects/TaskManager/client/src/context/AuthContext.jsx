e40e9df61fc71334db49ee5501933f6ad0c8ce04
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 14:07:10 -0300

    corregimos el error cambiando el message por el arreglo desde el backend

--- projects/TaskManager/client/src/context/AuthContext.jsx
+++ projects/TaskManager/client/src/context/AuthContext.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
       setUser(res.data);
       setIsAuthenticated(true);
     } catch (error) {
-      console.log(error.response);
+      // console.log(error.response);
       setErrors(error.response.data);
     }
   };
--- projects/TaskManager/server/src/controllers/auth.controllers.js
+++ projects/TaskManager/server/src/controllers/auth.controllers.js
@@ -7,8 +7,7 @@ export const register = async (req, res) => {
 
   try {
     const userFound = await User.findOne({ email });
-    if (userFound)
-      return res.status(400).json({ message: ["The email already is use"] });
+    if (userFound) return res.status(400).json(["The email already is use"]);
 
     const passwordHash = await bcrypt.hash(password, 10);
 
