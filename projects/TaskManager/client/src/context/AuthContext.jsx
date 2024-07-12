ed2605a7337c59f8460911009e4e1110bd717387
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 15:10:48 -0300

    configuramos el envio de cookies, pasamos al backend para que la guarde

--- projects/TaskManager/client/src/context/AuthContext.jsx
+++ projects/TaskManager/client/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import { registerRequest, loginRequest } from "../api/auth";
+import Cookies from "js-cookie";
 
 export const AuthContext = createContext();
 
@@ -50,6 +51,13 @@ export const AuthProvider = ({ children }) => {
       return () => clearTimeout(timer);
     }
   }, [errors]);
+
+  useEffect(() => {
+    const cookies = Cookies.get();
+    if (cookies.token) {
+      console.log(cookies.token);
+    }
+  }, []);
   return (
     <AuthContext.Provider
       value={{ signup, signin, user, isAuthenticated, errors }}
