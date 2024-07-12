505640e674c4ecd7889c1f4c24d1eecd760601c3
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 01:10:51 -0300

    configuramos el index para ejecutar la conexion y agregamos un mensaje para confirmar dicha conexion

--- projects/TaskManager/server/src/db.js
+++ projects/TaskManager/server/src/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://localhost/test-merndb");
+    console.log(">>> DB is connected");
   } catch (error) {
     console.error(error);
   }
