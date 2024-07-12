b76bfc75e78d3bd5e2de2f13f5324faacdaba41e
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 12:34:08 -0300

    configuramos el router con la finalidad de navegar entre paginas

--- projects/TaskManager/client/src/App.jsx
+++ projects/TaskManager/client/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}></Route>
+        <Route path="/login" element={<h1>Login</h1>}></Route>
+        <Route path="/register" element={<h1>Register</h1>}></Route>
+        <Route path="/tasks" element={<h1>Tasks Page</h1>}></Route>
+        <Route path="/add-task" element={<h1>New Task</h1>}></Route>
+        <Route path="/tasks/:id" element={<h1>Update Task</h1>}></Route>
+        <Route path="/profile" element={<h1>Profile</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
