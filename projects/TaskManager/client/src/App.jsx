5e7aad4d28a454ca9c3a908a63972ea6434c4303
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 12:38:48 -0300

    importamos los componentes en su route correspoindiente

--- projects/TaskManager/client/src/App.jsx
+++ projects/TaskManager/client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>}></Route>
-        <Route path="/login" element={<h1>Login</h1>}></Route>
-        <Route path="/register" element={<h1>Register</h1>}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/tasks" element={<h1>Tasks Page</h1>}></Route>
         <Route path="/add-task" element={<h1>New Task</h1>}></Route>
         <Route path="/tasks/:id" element={<h1>Update Task</h1>}></Route>
