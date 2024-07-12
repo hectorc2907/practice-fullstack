754b259d445b75acf32b23a95c44b03c17a6110f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 16:10:48 -0300

    configuramos el loading para que al recargar nos mande a la pagina que corresponde

--- projects/TaskManager/client/src/context/AuthContext.jsx
+++ projects/TaskManager/client/src/context/AuthContext.jsx
@@ -56,22 +56,25 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     async function checkLogin() {
       const cookies = Cookies.get();
-      if (cookies.token) {
-        try {
-          const res = await verifyTokenRequest(cookies.token);
-          if (!res.data) {
-            setIsAuthenticated(false);
-            setLoading(false);
-            return;
-          }
-          setIsAuthenticated(true);
-          setUser(res.data);
-          setLoading(false);
-        } catch (error) {
+      if (!cookies.token) {
+        setIsAuthenticated(false);
+        setLoading(false);
+        return setUser(null);
+      }
+      try {
+        const res = await verifyTokenRequest(cookies.token);
+        if (!res.data) {
           setIsAuthenticated(false);
-          setUser(null);
           setLoading(false);
+          return;
         }
+        setIsAuthenticated(true);
+        setUser(res.data);
+        setLoading(false);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser(null);
+        setLoading(false);
       }
     }
     checkLogin();
