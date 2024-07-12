be35ee344232eec06233a69622336325196dd490
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 11:21:45 -0300

    modificamos el modelo de task para que haga referencia al usuario que esta logueado y crea la task

--- projects/TaskManager/server/src/models/task.model.js
+++ projects/TaskManager/server/src/models/task.model.js
@@ -5,6 +5,7 @@ const taskSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: Date, default: Date.now },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   },
   {
     timestamps: true,
