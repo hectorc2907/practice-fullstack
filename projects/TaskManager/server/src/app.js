d87a4be8738e86130c003685c7a425a9441ce39a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 15:22:15 -0300

    configuramos el cors en el backend para poder acceder a las rutas

--- projects/TaskManager/server/src/app.js
+++ projects/TaskManager/server/src/app.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
