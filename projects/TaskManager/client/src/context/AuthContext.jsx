3b8c73ad8f6eacb27efcb68b159e940caa7cd1a5
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 15:07:10 -0300

    configuramos el login para que el setUser y setIsAuthenticated pasen a tener valores

--- projects/TaskManager/client/src/context/AuthContext.jsx
+++ projects/TaskManager/client/src/context/AuthContext.jsx
@@ -32,6 +32,8 @@ export const AuthProvider = ({ children }) => {
     try {
       const res = await loginRequest(user);
       console.log(res);
+      setUser(res.data);
+      setIsAuthenticated(true);
     } catch (error) {
       if (Array.isArray(error.response.data)) {
         return setErrors(error.response.data);
