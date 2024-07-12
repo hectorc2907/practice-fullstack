c20a32ef63de1d109b3de7865c859ad2f9dadc88
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 10:23:06 -0300

    creamos el middleware validateToken con la finalidad de verificar el login antes de enseñar la informacion del perfil

--- projects/TaskManager/server/src/routes/auth.routes.js
+++ projects/TaskManager/server/src/routes/auth.routes.js
@@ -5,12 +5,13 @@ import {
   profile,
   register,
 } from "../controllers/auth.controllers.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/profile", profile);
+router.get("/profile", authRequired, profile);
 
 export default router;
