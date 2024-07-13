97ada648ee16ad88faa32e30114fbbaa5f3176f2
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 23:25:34 -0300

    estilamos las cajas que contienen las tareas

---
+++ projects/TaskManager/client/src/components/TaskCard.jsx
@@ -0,0 +1,15 @@
+function TaskCard({ task }) {
+  return (
+    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+      <h1 className="text-2xl font-bold">{task.title}</h1>
+      <div>
+        <button>update</button>
+        <button>delete</button>
+      </div>
+      <p className="text-slate-300">{task.description}</p>
+      <p>{new Date(task.date).toLocaleDateString()}</p>
+    </div>
+  );
+}
+
+export default TaskCard;
