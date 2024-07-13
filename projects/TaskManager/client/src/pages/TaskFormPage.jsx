3844c4c2aa6b22a90168ed3d7ac085597ef7ce6d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-13 00:35:57 -0300

    configuramos los formatos de date

--- projects/TaskManager/client/src/pages/TaskFormPage.jsx
+++ projects/TaskManager/client/src/pages/TaskFormPage.jsx
@@ -2,9 +2,9 @@ import { useForm } from "react-hook-form";
 import { useTasks } from "../context/TasksContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
-import day from "dayjs";
+import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
-day.extend(utc);
+dayjs.extend(utc);
 
 function TaskFormPage() {
   const { register, handleSubmit, setValue } = useForm();
@@ -19,16 +19,21 @@ function TaskFormPage() {
         console.log(task);
         setValue("title", task.title);
         setValue("description", task.description);
+        setValue("date", dayjs.utc(task.date).format("YYYY-MM-DD"));
       }
     }
     loadTask();
   }, []);
 
   const onSubmit = handleSubmit((data) => {
+    const dataValid = {
+      ...data,
+      date: data.date ? dayjs.utc(data.date).format() : dayjs.utc().format(),
+    };
     if (params.id) {
-      updateTask(params.id, { ...data, date: day.utc(data.date).format() });
+      updateTask(params.id, dataValid);
     } else {
-      createTask({ ...data, date: day.utc(data.date).format() });
+      createTask(dataValid);
     }
     navigate("/tasks");
   });
