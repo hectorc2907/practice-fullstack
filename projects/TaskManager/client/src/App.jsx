c2fe15301fd6e2a4405522e1ddc0822ec3105cf3
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 23:27:44 -0300

    estilamos el margen principal en pantallas grandes

--- projects/TaskManager/client/src/App.jsx
+++ projects/TaskManager/client/src/App.jsx
@@ -15,18 +15,20 @@ function App() {
     <AuthProvider>
       <TaskProvider>
         <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="/register" element={<RegisterPage />}></Route>
-            <Route element={<ProtectedRoute />}>
-              <Route path="/tasks" element={<TasksPage />}></Route>
-              <Route path="/add-task" element={<TaskFormPage />}></Route>
-              <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
-              <Route path="/profile" element={<ProfilePage />}></Route>
-            </Route>
-          </Routes>
+          <main className="container mx-auto px-10">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="/login" element={<LoginPage />}></Route>
+              <Route path="/register" element={<RegisterPage />}></Route>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/tasks" element={<TasksPage />}></Route>
+                <Route path="/add-task" element={<TaskFormPage />}></Route>
+                <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
+                <Route path="/profile" element={<ProfilePage />}></Route>
+              </Route>
+            </Routes>
+          </main>
         </BrowserRouter>
       </TaskProvider>
     </AuthProvider>
