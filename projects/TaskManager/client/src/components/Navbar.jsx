c99b3883ccd01207265b719a296fc5d740abdc9b
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 23:09:23 -0300

    realizamos el navbar condicional al momento de loguearnos

--- projects/TaskManager/client/src/components/Navbar.jsx
+++ projects/TaskManager/client/src/components/Navbar.jsx
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
+  const { isAuthenticated } = useAuth();
   return (
     <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
       <Link to="/">
         <h1 className="text-2xl font-bold">Tasks Manager</h1>
       </Link>
       <ul className="flex gap-x-2">
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {isAuthenticated ? (
+          <>
+            <li>Welcome User</li>
+            <li>
+              <Link to="/add-task">Add Task</Link>
+            </li>
+            <li>
+              <Link to="/add-task">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
