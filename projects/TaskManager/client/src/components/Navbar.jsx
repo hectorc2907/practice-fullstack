972a86f2002d6f39a079d6e5055fb3485046ce57
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 23:16:56 -0300

    estilamos los botones del navbar

--- projects/TaskManager/client/src/components/Navbar.jsx
+++ projects/TaskManager/client/src/components/Navbar.jsx
@@ -13,7 +13,12 @@ function Navbar() {
           <>
             <li>Welcome {user.username}</li>
             <li>
-              <Link to="/add-task">Add Task</Link>
+              <Link
+                to="/add-task"
+                className="bg-indigo-500 px-4 py-1 rounded-sm"
+              >
+                Add Task
+              </Link>
             </li>
             <li>
               <Link
@@ -29,10 +34,17 @@ function Navbar() {
         ) : (
           <>
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/login" className="bg-indigo-500 px-4 py-1 rounded-sm">
+                Login
+              </Link>
             </li>
             <li>
-              <Link to="/register">Register</Link>
+              <Link
+                to="/register"
+                className="bg-indigo-500 px-4 py-1 rounded-sm"
+              >
+                Register
+              </Link>
             </li>
           </>
         )}
