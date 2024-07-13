4919d6a488285ac95db30daf7a4e69ba33b10601
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-13 00:50:47 -0300

    configuramos las variables de entorno del backend

--- projects/TaskManager/server/src/app.js
+++ projects/TaskManager/server/src/app.js
@@ -2,11 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
+
+dotenv.config();
+
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.ORIGIN_FRONTEND, credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
