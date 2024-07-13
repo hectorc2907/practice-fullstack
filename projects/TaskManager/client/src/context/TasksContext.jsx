944c91d9b04a45bcdb0a36aa3114e9ac291a8eee
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 22:34:34 -0300

    generamos en el taskcontext la recuperacion de tareas del usuario

--- projects/TaskManager/client/src/context/TasksContext.jsx
+++ projects/TaskManager/client/src/context/TasksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { createTaskRequest } from "../api/tasks";
+import { getTasksRequest, createTaskRequest } from "../api/tasks";
 
 const TaskContext = createContext();
 
@@ -14,13 +14,18 @@ export const useTasks = () => {
 export function TaskProvider({ children }) {
   const [tasks, setTasks] = useState([]);
 
+  const getTasks = async () => {
+    const res = await getTasksRequest();
+    console.log(res);
+  };
+
   const createTask = async (task) => {
     const res = await createTaskRequest(task);
     console.log(res);
   };
 
   return (
-    <TaskContext.Provider value={{ tasks, createTask }}>
+    <TaskContext.Provider value={{ tasks, getTasks, createTask }}>
       {children}
     </TaskContext.Provider>
   );
--- projects/TaskManager/client/src/pages/TasksPage.jsx
+++ projects/TaskManager/client/src/pages/TasksPage.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import { useTasks } from "../context/TasksContext";
+
 function TasksPage() {
+  const { getTasks } = useTasks();
+
+  useEffect(() => {
+    getTasks();
+  }, []);
   return <div>TasksPage</div>;
 }
 
