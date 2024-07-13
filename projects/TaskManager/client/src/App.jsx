71f6b1630f4913c53fdacfe14c7341d1062c4278
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 22:04:05 -0300

    se realiza la creacion del TaskContext y su implementacion en el App.jsx

--- projects/TaskManager/client/src/App.jsx
+++ projects/TaskManager/client/src/App.jsx
@@ -7,23 +7,26 @@ import ProtectedRoute from "./ProtectedRoute";
 import TasksPage from "./pages/TasksPage";
 import TaskFormPage from "./pages/TaskFormPage";
 import ProfilePage from "./pages/ProfilePage";
+import { TaskProvider } from "./context/TasksContext";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
-          <Route element={<ProtectedRoute />}>
-            <Route path="/tasks" element={<TasksPage />}></Route>
-            <Route path="/add-task" element={<TaskFormPage />}></Route>
-            <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
-            <Route path="/profile" element={<ProfilePage />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <TaskProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/register" element={<RegisterPage />}></Route>
+            <Route element={<ProtectedRoute />}>
+              <Route path="/tasks" element={<TasksPage />}></Route>
+              <Route path="/add-task" element={<TaskFormPage />}></Route>
+              <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
+              <Route path="/profile" element={<ProfilePage />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TaskProvider>
     </AuthProvider>
   );
 }
---
+++ projects/TaskManager/client/src/context/TasksContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+const TaskContext = createContext();
+
+export const useTasks = () => {
+  const context = useContext(TaskContext);
+  if (!context) {
+    throw new Error("useTasks must be used within a TaskProvider");
+  }
+  return context;
+};
+
+export function TaskProvider({ children }) {
+  return <TaskContext value={{}}>{children}</TaskContext>;
+}
