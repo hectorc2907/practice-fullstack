c2273f94916687687b4105c8887b7af47bf96a5d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-12 21:59:47 -0300

    estilamos el taskFormPage

--- projects/TaskManager/client/src/pages/TaskFormPage.jsx
+++ projects/TaskManager/client/src/pages/TaskFormPage.jsx
@@ -7,21 +7,26 @@ function TaskFormPage() {
     console.log(data);
   });
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Title"
-          {...register("title")}
-          autoFocus
-        />
-        <textarea
-          rows="3"
-          placeholder="Description"
-          {...register("description")}
-        ></textarea>
-        <button type="submit">Save</button>
-      </form>
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <h1 className="text-2xl font-bold">Task</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="Title"
+            {...register("title")}
+            autoFocus
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          />
+          <textarea
+            rows="3"
+            placeholder="Description"
+            {...register("description")}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          ></textarea>
+          <button type="submit">Save</button>
+        </form>
+      </div>
     </div>
   );
 }
