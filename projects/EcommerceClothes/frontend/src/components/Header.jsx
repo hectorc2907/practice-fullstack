8eaa1ec9b8196e7e3919c606d141b416c2e84bcb
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-24 19:59:52 -0300

    configuramos el react router dom en app y creamos el componente Home con su direccion

--- projects/EcommerceClothes/frontend/src/components/Header.jsx
+++ projects/EcommerceClothes/frontend/src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
 const Header = () => {
   return (
     <header>
-      <div></div>
+      <div>
+        <div>
+          <Link>
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 };
