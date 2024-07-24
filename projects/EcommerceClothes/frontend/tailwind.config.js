b1323addcdf6766afe3a04cfe71e20bceec6245e
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-24 00:04:22 -0300

    se realiza la configuracion de los estilos y del tailwind.config.js

--- projects/EcommerceClothes/frontend/tailwind.config.js
+++ projects/EcommerceClothes/frontend/tailwind.config.js
@@ -2,7 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f0f2f5",
+        secondary: "#ff813f",
+        tertiary: "#2222222",
+        slate: {
+          10: "#f1f3f4",
+        },
+        green: {
+          50: "#30af58",
+          90: "#292c27",
+        },
+        gray: {
+          10: "#eeeeee",
+          20: "#a2a2a2",
+          30: "#7b7b7b",
+          50: "#585858",
+          90: "#141414",
+        },
+      },
+      backgroundImage: {
+        hero: "url('/src/assets/bgecom.png')",
+        banneroffer: "url('/src/assets/banneroffer.png')",
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10x1": "1512px",
+      },
+      borderColor: {
+        "5xl": "40px",
+      },
+    },
   },
   plugins: [],
 };
