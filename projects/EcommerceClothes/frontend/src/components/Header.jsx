a04e1983dec58d4bd8f33c1ec82ffbefb910f210
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-24 20:16:21 -0300

    verificamos el funcionamiento de los estilos y reinstalamos tailwindcss

--- projects/EcommerceClothes/frontend/src/components/Header.jsx
+++ projects/EcommerceClothes/frontend/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header>
+    <header className="fixed top-0 left-0 m-auto mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-">
       <div>
         <div>
           <Link>
             <img src={logo} alt="" height={66} width={66} />
           </Link>
         </div>
+        <Navbar />
+        <div>buttons</div>
       </div>
     </header>
   );
